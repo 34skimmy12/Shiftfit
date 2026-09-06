fdc68ee1e622e97477405d7f6af7387c0ed20a72
34skimmy12 <108073523+34skimmy12@users.noreply.github.com>
2026-09-06 15:24:15 +0100

    Load upgraded Help & Support UI v2

--- remove-progress-placeholder.js
+++ remove-progress-placeholder.js
@@ -24,7 +24,7 @@
     loadScript("shiftfit-settings-back-fix-loader","./shiftfit-settings-back-fix.js?v=1");
     loadScript("shiftfit-account-settings-loader","./shiftfit-account-settings.js?v=1");
     loadScript("shiftfit-data-privacy-loader","./shiftfit-data-privacy.js?v=1");
-    loadScript("shiftfit-support-ai-loader","./shiftfit-support-ai.js?v=1");
+    loadScript("shiftfit-support-ai-loader","./shiftfit-support-ai.js?v=2");
     loadScript("shiftfit-gmail-support-loader","./shiftfit-gmail-support.js?v=1");
     loadScript("shiftfit-settings-compact-loader","./shiftfit-settings-compact.js?v=3");
     setTimeout(removePlaceholder,500);
