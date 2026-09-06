b8d389ee5ce20ee7a79f5f272b33ca7742ae4adf
34skimmy12 <108073523+34skimmy12@users.noreply.github.com>
2026-09-06 22:56:24 +0100

    Wire Profile Control Centre into app

--- remove-progress-placeholder.js
+++ remove-progress-placeholder.js
@@ -17,6 +17,7 @@
   }
   function boot(){
     removePlaceholder();
+    loadScript("shiftfit-profile-control-center-loader","./profile-control-center.js?v=1");
     loadScript("shiftfit-profile-plan-bridge-loader","./profile-plan-regeneration-bridge.js?v=5");
     loadScript("shiftfit-avatar-sync-loader","./shiftfit-avatar-sync.js?v=1");
     loadScript("shiftfit-notifications-loader","./shiftfit-notifications.js?v=1");
